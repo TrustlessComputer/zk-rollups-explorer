efe88a1997ce1a7fef522637ce6da9552a5810bc
tony <tony@bvm.network>
2024-09-10 18:34:49 +0700

    Add compare False string when Testnet

--- scripts/setup-hyperchain-config.ts
+++ scripts/setup-hyperchain-config.ts
@@ -6,7 +6,7 @@ import { parse as parseConnectionString } from 'pg-connection-string';
 
 const buildAppConfig = (zkSyncEnvs: { [key: string]: string }) => {
   const domain = zkSyncEnvs.CHAIN_ETH_ZKSYNC_NETWORK_URL || '';
-  const isMainnet = zkSyncEnvs.IS_MAINNET;
+  const isMainnet = zkSyncEnvs.IS_MAINNET === 'False';
   const apiUrl = `https://api.${domain}`;
   const verificationApiUrl = `https://contractverification.${domain}`;
   const rpcUrl = `https://rpc.${domain}`;
